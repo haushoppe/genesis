0d9090f4016f682e67b7664561c67e27788ab621
Hans Crypto <johannes@haushoppe.art>
2022-05-01 19:23:15 +0200

    own types for the Open Api CLI Plugin

--- cube-workspace/apps/backend/src/app/api/api.controller.ts
+++ cube-workspace/apps/backend/src/app/api/api.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { NftCollection } from './moralis-openapi-types.dto';
 
 const Moralis = require('moralis/node');
-
-class ShortNft {
-
-}
 
 @Controller()
 export class ApiController {
@@ -17,17 +14,39 @@ export class ApiController {
   }
 
 
-  // http://localhost:3333/api/get-nfts-of-owner/0x8c11C53F77aD5e91fB13611904f2F59b07Aa7c93
-  @Get(['api/get-nfts-of-owner/:address'])
-  async getNftsOfOwner(@Param('address') address: string) {
-
+  /**
+   * Returns a list of all NFTs that the given address has
+   */
+  // http://localhost:3333/api/get-nfts-for-address/0x8c11C53F77aD5e91fB13611904f2F59b07Aa7c93
+  @Get(['api/get-nfts-for-address/:address'])
+  async getNftsForAddress(@Param('address') address: string): Promise<NftCollection> {
 
     const options = { address };
-    const nfts = await Moralis.Web3API.account.getNFTs(options);
+    const nftsRaw: NftCollection = await Moralis.Web3API.account.getNFTs(options);
 
-    return {
-      address,
-      nfts
-    }
+    console.log(typeof nftsRaw);
+
+    // // remove all unnecessary other properties
+    // const nfts = nftsRaw.map(({
+    //   amount,
+    //   contract_type,
+    //   name,
+    //   symbol,
+    //   token_address,
+    //   token_id,
+    //   token_uri,
+    //   metadata
+    // }) => ({
+    //   amount,
+    //   contract_type,
+    //   name,
+    //   symbol,
+    //   token_address,
+    //   token_id,
+    //   token_uri,
+    //   metadata
+    // }));
+
+    return nftsRaw;
   }
 }

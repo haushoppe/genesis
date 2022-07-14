524641480f08239e11af1848b478d0ac925af7da
Hans Crypto <johannes@haushoppe.art>
2022-07-14 11:03:04 +0200

    + routing

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ScalesComponent } from './scales/scales.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot([
+      { path: '', component: StartComponent },
+      { path: 'scales', component: ScalesComponent }
+    ],
+    { initialNavigation: 'enabledBlocking' }),
     HeaderComponent,
     FooterComponent
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

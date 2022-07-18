482f83991ffc8fe477c7b0ca207398447b86da6c
Ethspresso <ethspresso@users.noreply.github.com>
2022-07-18 21:41:37 +0200

    Update all Scales to schema v2 with owner objects

--- crawler/rescan_fix_all.py
+++ crawler/rescan_fix_all.py
@@ -1,47 +1,112 @@
 import os
 import sys
 import json
+import math
+import pymongo
 import logging
 import requests
+import datetime
 
-from dateutil import parser
+from dotenv import load_dotenv
 
-import pymongo
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-from dotenv import load_dotenv
+from config import DAO_MEMBERS
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', stream=sys.stdout)
 
 
-DAO_MEMBERS = [
-    # Add lowercased wallet addresses here
-]
+def get_owner_id(address, owner_db):
+    """
+    Look up wallet in list and return ID of owner. Create if not found.
+
+    Params:
+        address (str): Address of wallet to look up
+        owner_db (pymongo.collection): Collection of owners
+
+    Returns:
+        str: ID of owner
+    """
+    owner = owner_db.find_one({"wallets": address.lower()})
+    if owner:
+        owner_id = owner['_id']
+    else:
+        owner_entry = {
+            "wallets": [address.lower()]
+        }
+        res = owner_db.insert_one(owner_entry)
+        owner_id = res.inserted_id
+
+    return owner_id
 
 
+def initialize_new_entry(asset, metadata_path, owner_db):
+    try:
+        metadata = json.load(open(os.path.join(metadata_path, f"{asset['token_id']}.json")))
+    except Exception as e:
+        raise ValueError(f"Unable to open metadata file for {asset['token_id']} due to {e}")
 
-def check_update_asset(db_client, asset):
-    db = db_client.scales
-    coll = db.scales
+    rows = ["A", "B", "C", "D", "E", "F"]
+    owner_id = get_owner_id(asset['owner']['address'].lower(), owner_db)
 
-    doc = coll.find_one({"id": asset["token_id"]})
+    doc = {
+        "token_id": int(asset["token_id"]),
+        "name": asset["name"],
+        "image_url": asset["image_original_url"],
+        "image_thumbnail_url": asset["image_thumbnail_url"],
+        "contract_address": asset["asset_contract"]["address"].lower(),
+        "painting": metadata["attributes"][0]["value"],
+        "position": {
+            "name": metadata["Row"] + metadata["Column"],
+            "row": rows.index(metadata["Row"]) + 1,
+            "col": int(metadata["Column"]),
+            "row_count": int(metadata["Parent Painting Row Amount"]),
+            "col_count": int(metadata["Parent Painting Column Amount"]),
+            "index": (rows.index(metadata["Row"]) * int(metadata["Parent Painting Column Amount"])) + int(metadata["Column"]) - 1,
+        },
+        "owner": {
+            "address": asset["owner"]["address"].lower(),
+            "owner_id": owner_id
+        },
+        "last_updated": {
+            "timestamp": datetime.datetime.now()
+        }
+    }
+
+    return doc
+
+
+def check_update_asset(db_client, asset, metadata_path):
+    mongodb = db_client.scales
+    scales_db = mongodb.scales
+    owner_db = mongodb.owner
+
+    token_id = int(asset["token_id"])
+
+    doc = scales_db.find_one({"token_id": token_id})
+
+    # Add new entry if it doesn't exist
     if doc is None:
-        logging.warning(f"Token {asset['token_id']} not found in database, skipped!")
-        logging.debug(asset)
-        return False
-    
-    if 'image_thumbnail_url' not in doc:
-        logging.info(f"Token {asset['token_id']} has no image_thumbnail_url, updating!")
-        coll.update_one({"id": asset["token_id"]}, {"$set": {"image_thumbnail_url": asset["image_thumbnail_url"]}})
+        doc = initialize_new_entry(asset, metadata_path, owner_db)
+        scales_db.insert_one(doc)
+        return
 
-    if 'owner' not in doc or doc['owner'].lower() != asset['owner']['address'].lower():
-        logging.info(f"Token {asset['token_id']} has incorrect owner details, updating!")
-        coll.update_one({"id": asset["token_id"]}, {"$set": {
-            "owner": asset["owner"]["address"].lower(),
-            "owned_by_us": asset["owner"]["address"].lower() in DAO_MEMBERS,
-            "owner_username": asset["owner"]["username"] if "username" in asset["owner"] else None,
-            }})
+    # Add image thumbnail URL if not already present    
+    if 'image_thumbnail_url' not in doc:
+        logging.info(f"Token {token_id} has no image_thumbnail_url, updating!")
+        scales_db.update_one({"id": token_id}, {"$set": {"image_thumbnail_url": asset["image_thumbnail_url"]}})
+
+    # Update owner details
+    if doc['owner']['address'].lower() != asset['owner']['address'].lower() or 'owner_id' not in doc['owner']:
+        logging.info(f"Token {token_id} has incorrect owner details, updating!")
+        owner_id = get_owner_id(asset['owner']['address'].lower(), owner_db)
+
+        scales_db.update_one({"token_id": token_id}, {"$set": {
+            "owner": {
+                "address": asset['owner']['address'].lower(),
+                "owner_id": owner_id
+            }}})
 
 
 if __name__ == '__main__':
@@ -64,7 +129,7 @@ if __name__ == '__main__':
         print(f"Error connecting to MongoDB: {e}")
         sys.exit(1)
 
-    url = f"https://api.opensea.io/api/v1/assets?order_direction=desc&asset_contract_address={CONTRACT_ADDRESS}&limit=20&include_orders=false"
+    url = f"https://api.opensea.io/api/v1/assets?order_direction=desc&asset_contract_address={CONTRACT_ADDRESS}&limit=10&include_orders=false"
 
     headers = {
         "Accept": "application/json",
@@ -76,12 +141,12 @@ if __name__ == '__main__':
 
     logging.info(f"Got {len(data['assets'])} assets")
     for asset in data['assets']:
-        check_update_asset(db_client, asset)
+        check_update_asset(db_client, asset, METADATA_PATH)
 
     while data['next']:
-        url = f"https://api.opensea.io/api/v1/assets?order_direction=desc&asset_contract_address={CONTRACT_ADDRESS}&limit=20&include_orders=false&cursor={data['next']}"
+        url = f"https://api.opensea.io/api/v1/assets?order_direction=desc&asset_contract_address={CONTRACT_ADDRESS}&limit=50&include_orders=false&cursor={data['next']}"
         response = requests.get(url, headers=headers)
         data = response.json()
         logging.info(f"Got {len(data['assets'])} more assets")
         for asset in data['assets']:
-            check_update_asset(db_client, asset)
+            check_update_asset(db_client, asset, METADATA_PATH)

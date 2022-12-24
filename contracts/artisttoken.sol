85338cd4b9ca61b81ac77c21c5014406496b14b8
Hans Crypto <johannes@haushoppe.art>
2022-12-24 23:17:02 +0100

    changing name + symbol
    as seen here https://github.com/chiru-labs/ERC721A/issues/375#issuecomment-1186185619

--- contracts/artisttoken.sol
+++ contracts/artisttoken.sol
@@ -417,12 +417,50 @@ contract ArtistToken is ERC721A, ReentrancyGuard, Ownable, Pausable {
         emit Agreement(msg.sender, false);
     }
 
+    // Contract code size exceeds 24576 byte
+    // /**
+    //  * Returns the agreement status of an address
+    //  */
+    // function agreementsStatus(address addr) public view returns (bool) {
+    //     // Query for the zero address
+    //     require(addr != address(0), "Query for 0x0");
+    //     return agreements[addr];
+    // }
+
+    // ******************** //
+    // Change name / symbol //
+    // ******************** //
+
+    function _setStringAtStorageSlot(string memory value, uint256 storageSlot) private {
+        assembly {
+            let stringLength := mload(value)
+
+            switch gt(stringLength, 0x1F)
+            case 0 {
+                sstore(storageSlot, or(mload(add(value, 0x20)), mul(stringLength, 2)))
+            }
+            default {
+                sstore(storageSlot, add(mul(stringLength, 2), 1))
+                mstore(0x00, storageSlot)
+                let dataSlot := keccak256(0x00, 0x20)
+                for { let i := 0 } lt(mul(i, 0x20), stringLength) { i := add(i, 0x01) } {
+                    sstore(add(dataSlot, i), mload(add(value, mul(add(i, 1), 0x20))))
+                }
+            }
+        }
+    }
+
     /**
-     * Returns the agreement status of an address
+     * @notice Change token name
      */
-    function agreementsStatus(address addr) public view returns (bool) {
-        // Query for the zero address
-        require(addr != address(0), "Query for 0x0");
-        return agreements[addr];
+    function setName(string memory value) external onlyOwner {
+        _setStringAtStorageSlot(value, 2);
+    }
+
+    /**
+     * @notice Change token symbol
+     */
+    function setSymbol(string memory value) external onlyOwner {
+        _setStringAtStorageSlot(value, 3);
     }
 }
--- contracts/cubetoken.sol
+++ contracts/cubetoken.sol
@@ -348,4 +348,41 @@ contract CubeToken is ERC721A, ReentrancyGuard, Ownable, Pausable {
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
+
+    // ******************** //
+    // Change name / symbol //
+    // ******************** //
+
+    function _setStringAtStorageSlot(string memory value, uint256 storageSlot) private {
+        assembly {
+            let stringLength := mload(value)
+
+            switch gt(stringLength, 0x1F)
+            case 0 {
+                sstore(storageSlot, or(mload(add(value, 0x20)), mul(stringLength, 2)))
+            }
+            default {
+                sstore(storageSlot, add(mul(stringLength, 2), 1))
+                mstore(0x00, storageSlot)
+                let dataSlot := keccak256(0x00, 0x20)
+                for { let i := 0 } lt(mul(i, 0x20), stringLength) { i := add(i, 0x01) } {
+                    sstore(add(dataSlot, i), mload(add(value, mul(add(i, 1), 0x20))))
+                }
+            }
+        }
+    }
+
+    /**
+     * @notice Change token name
+     */
+    function setName(string memory value) external onlyOwner {
+        _setStringAtStorageSlot(value, 2);
+    }
+
+    /**
+     * @notice Change token symbol
+     */
+    function setSymbol(string memory value) external onlyOwner {
+        _setStringAtStorageSlot(value, 3);
+    }
 }

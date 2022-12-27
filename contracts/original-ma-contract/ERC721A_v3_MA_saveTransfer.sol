1782c73bb33e2122d3948856404cff83c170b064
Hans Crypto <johannes@haushoppe.art>
2022-12-27 20:47:29 +0100

    adding original MA contract

---
+++ contracts/original-ma-contract/ERC721A_v3_MA_saveTransfer.sol
@@ -0,0 +1,44 @@
+    /**
+     * @dev Safe Transfers `tokenId` from `from` to `to` without checking for explicit ownership approval
+     * equivalent to _safeTransfer from OZ
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal {
+        TokenOwnership memory prevOwnership = ownershipOf(tokenId);
+
+        
+	
+	require(prevOwnership.addr == from, "ERC721A: transfer from incorrect owner");
+        require(to != address(0), "ERC721A: transfer to the zero address");
+
+        _beforeTokenTransfers(from, to, tokenId, 1);
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId, prevOwnership.addr);
+
+        _addressData[from].balance -= 1;
+        _addressData[to].balance += 1;
+        _ownerships[tokenId] = TokenOwnership(to, uint64(block.timestamp));
+
+        // If the ownership slot of tokenId+1 is not explicitly set, that means the transfer initiator owns it.
+        // Set the slot of tokenId+1 explicitly in storage to maintain correctness for ownerOf(tokenId+1) calls.
+        uint256 nextTokenId = tokenId + 1;
+        if (_ownerships[nextTokenId].addr == address(0)) {
+            if (_exists(nextTokenId)) {
+                _ownerships[nextTokenId] = TokenOwnership(prevOwnership.addr, prevOwnership.startTimestamp);
+            }
+        }
+
+        emit Transfer(from, to, tokenId);
+        _afterTokenTransfers(from, to, tokenId, 1);
+    }

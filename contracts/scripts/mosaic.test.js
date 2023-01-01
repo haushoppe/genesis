98994796ea4f16e6e862050b6fd16c5eb0889eb7
Hans Crypto <johannes@haushoppe.art>
2023-01-01 12:00:34 +0100

    more mosaic tests

--- contracts/scripts/mosaic.test.js
+++ contracts/scripts/mosaic.test.js
@@ -86,9 +86,21 @@ const { deployToken } = require("./_utils");
       // this should be already covered, but just to be super sure
       await expect(token.mintMosaic(1, 1, 1, 3)).to.be.revertedWith('All tokens for a mosaic must be unique');
       await expect(token.mintMosaic(1, 1, 1, 1)).to.be.revertedWith('All tokens for a mosaic must be unique');
+
+      // make sure that the tx was reverted and the supply and mosaics are not in a bogus state
+      expect(await token.totalSupply()).to.equal(4, 'There must be still only 4 tokens now!');
+
+      expect((await token.mosaics(0)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(1)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(2)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(3)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(4)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(5)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(6)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
+      expect((await token.mosaics(7)).map(x => x.toString())).to.eql(["0", "0", "0", "0"]);
     });
 
-    it('should be able to mint mosaics in a batch', async () => {
+    it('should be able to mint 3 mosaics in a batch', async () => {
 
       // user mints 12 tokens for 3 mosaics
       await token.mint(12);
@@ -111,6 +123,50 @@ const { deployToken } = require("./_utils");
       expect(await token.tokenURI(12)).to.equal('https://example-mosaic.org/12/0/1/2/3');
       expect(await token.tokenURI(13)).to.equal('https://example-mosaic.org/13/4/5/6/7');
       expect(await token.tokenURI(14)).to.equal('https://example-mosaic.org/14/8/9/10/11');
+    });
+
+    it('should be able to mint 1, 2 and 4 mosaics in a batch', async () => {
+
+      // user mints 28 tokens
+      await token.mint(28);
+
+      // mint a 1 mosaic - new tokenId is #28 --- this is not effective, but should be possible
+      await token.mintMosaicBatch(
+        [ 0, 1, 2, 3 ],
+        [ 0, 0, 0, 0 ], // skipped
+        [ 0, 0, 0, 0 ], // skipped
+        [ 0, 0, 0, 0 ] // skipped
+      );
+
+      expect(await token.tokenURI(28)).to.equal('https://example-mosaic.org/28/0/1/2/3');
+
+      // mint a 2 mosaics - new tokenIds are #29, #30
+      await token.mintMosaicBatch(
+        [ 4, 5, 6, 7 ],
+        [ 8, 9, 10, 11],
+        [ 0, 0, 0, 0 ], // skipped
+        [ 0, 0, 0, 0 ] // skipped
+      );
+
+      expect(await token.tokenURI(29)).to.equal('https://example-mosaic.org/29/4/5/6/7');
+      expect(await token.tokenURI(30)).to.equal('https://example-mosaic.org/30/8/9/10/11');
+
+
+      // mint a 4 mosaics - new tokenIds are #31, #32, #33, #34
+      await token.mintMosaicBatch(
+        [ 12, 13, 14, 15 ],
+        [ 16, 17, 18, 19 ],
+        [ 20, 21, 22, 23 ],
+        [ 24, 25, 26, 27 ]
+      );
+
+      expect(await token.tokenURI(31)).to.equal('https://example-mosaic.org/31/12/13/14/15');
+      expect(await token.tokenURI(32)).to.equal('https://example-mosaic.org/32/16/17/18/19');
+      expect(await token.tokenURI(33)).to.equal('https://example-mosaic.org/33/20/21/22/23');
+      expect(await token.tokenURI(34)).to.equal('https://example-mosaic.org/34/24/25/26/27');
+
+
+      expect(await token.totalSupply()).to.equal(35, 'There must be 35 tokens now!');
     });
 
     it('should NOT be possible to use a tile twice in a batch mint', async () => {

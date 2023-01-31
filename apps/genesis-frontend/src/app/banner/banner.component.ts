f974875ab4c388a476eec3f57091ea8d8eeeb5ac
Hans Crypto <johannes@haushoppe.art>
2023-01-31 11:35:38 +0100

    frontend: cool random image

--- apps/genesis-frontend/src/app/banner/banner.component.ts
+++ apps/genesis-frontend/src/app/banner/banner.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
-  standalone: true
+  standalone: true,
+  encapsulation: ViewEncapsulation.None
 })
 export class BannerComponent {
+
+  bannerImages = [
+    'https://assets.haushoppe.art/genesis/genesis1/7colors.jpg',
+    'https://assets.haushoppe.art/genesis/genesis2/5colors.jpg',
+    'https://assets.haushoppe.art/genesis/genesis3/6colors.jpg',
+    'https://assets.haushoppe.art/genesis/genesis4/5colors.jpg'
+  ];
+
+  randomImage = this.bannerImages[Math.floor(Math.random() * this.bannerImages.length)];
+
+  @HostBinding('attr.style')
+  get myStyle(): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(`--banner-image: url("${ this.randomImage }")`);
+  }
+
+  constructor(private sanitizer: DomSanitizer) {}
 }

c8eb6a4fb084429ec768d8950443e90194240d27
Hans Crypto <johannes@haushoppe.art>
2023-10-11 23:28:26 +0200

    Presskit!!

--- apps/cubes-frontend/src/app/ordinal.routes.ts
+++ apps/cubes-frontend/src/app/ordinal.routes.ts
@@ -4,12 +4,14 @@ import { StartComponent } from './start/start.component';
 import { FaqComponent } from './faq/faq.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 import { OrderConnectDetailsComponent } from './order-connect-details/order-connect-details.component';
+import { PresskitComponent } from './presskit/presskit.component';
 
 export const ORDINAL_ROUTES: Routes = [
   { path: '', component: StartComponent },
   { path: 'mint/:collectionSymbol', component: StartComponent },
   // { path: 'nft/:tokenId', component: DetailsComponent }
   { path: 'faq', component: FaqComponent },
+  { path: 'presskit', component: PresskitComponent },
   { path: 'order/:orderId', component: OrderDetailsComponent, data: { hideBanner: true } },
   { path: 'order-connect/:txId', component: OrderConnectDetailsComponent, data: { hideBanner: true } },
   { path: '**', component: StartComponent },
---
+++ apps/cubes-frontend/src/app/presskit/presskit.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-presskit',
+    templateUrl: './presskit.component.html',
+    styleUrls: ['./presskit.component.scss'],
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+
+    ]
+})
+export class PresskitComponent {
+
+
+}
